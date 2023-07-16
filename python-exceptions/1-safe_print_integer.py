4a1199b81eabead154cd5a18df957b6cae80a8ce
bmukankusi <b.mukankusi@alustudent.com>
2023-07-16 13:03:49 -0700

    Fix task 0 and 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,17 +2,8 @@
 
 
 def safe_print_integer(value):
- """Print an integer with "{:d}".format().
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-   """
     try:
-        print("{:d}".format(value))
-      return (True)
-    except (TypeError, ValueError):
-      return (False)
+        print("{:d}".format(int(value)))
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
